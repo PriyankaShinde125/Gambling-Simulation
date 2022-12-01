934c4be839589ece52c1322eb6d822fc444e5bb7
“PriyankaShinde” <pkshinde125@gmail.com>
2022-12-01 23:22:30 +0530

    ADDED : Code to start with stake and bet

--- src/GamblingSimulator.java
+++ src/GamblingSimulator.java
@@ -1,4 +1,8 @@
 public class GamblingSimulator {
+
+    int stake = 100;
+    int bet = 1;
+
     public static void main(String[] args) {
         System.out.println("Welcome to gambling simulator...");
     }
