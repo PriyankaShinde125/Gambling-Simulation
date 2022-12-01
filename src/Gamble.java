14094542abc40e524d38cf3e7f207f1c76e803eb
“PriyankaShinde” <pkshinde125@gmail.com>
2022-12-02 00:04:07 +0530

    ADDED : Code to play till gambler win or loss 50% of total stake

--- src/Gamble.java
+++ src/Gamble.java
@@ -1,15 +1,32 @@
+import java.util.Stack;
+
 public class Gamble {
     public static final int WIN_BET = 1;
+    public final int MAX_STAKE;
+    public final int MIN_STAKE;
     int stake;
     int bet;
 
     public Gamble() {
         stake = 100;
         bet = 1;
+        MIN_STAKE = stake * 50 / 100;
+        MAX_STAKE = stake + MIN_STAKE;
     }
 
     public void makeABet() {
         int betResult = (int) Math.floor(Math.random() * 2);
-        System.out.println(betResult == WIN_BET ? "You win $" + bet : "You loss $" + bet);
+        System.out.println(betResult == WIN_BET ? "You won $" + bet : "You lost $" + bet);
+        if (betResult == WIN_BET)
+            stake += 1;
+        else
+            stake -= 1;
+    }
+
+    public void playTillMaxStake() {
+        while (stake < MAX_STAKE && stake > MIN_STAKE) {
+            makeABet();
+            System.out.println("Stake = " + stake);
+        }
     }
 }
--- src/GamblingSimulator.java
+++ src/GamblingSimulator.java
@@ -2,6 +2,6 @@ public class GamblingSimulator {
     public static void main(String[] args) {
         System.out.println("Welcome to gambling simulator...");
         Gamble gamble = new Gamble();
-        gamble.makeABet();
+        gamble.playTillMaxStake();
     }
 }
