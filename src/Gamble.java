cb10057f1cf0cb03601c8fa1c2e09f42124cc613
“PriyankaShinde” <pkshinde125@gmail.com>
2022-12-02 01:12:17 +0530

    ADDED : Code to play for 20 days and know daily total won/lost amount

--- src/Gamble.java
+++ src/Gamble.java
@@ -2,6 +2,7 @@ import java.util.Stack;
 
 public class Gamble {
     public static final int WIN_BET = 1;
+    public static final int MAX_DAYS = 20;
     public final int MAX_STAKE;
     public final int MIN_STAKE;
     int stake;
@@ -16,17 +17,35 @@ public class Gamble {
 
     public void makeABet() {
         int betResult = (int) Math.floor(Math.random() * 2);
-        System.out.println(betResult == WIN_BET ? "You won $" + bet : "You lost $" + bet);
+        // System.out.println(betResult == WIN_BET ? "You won $" + bet : "You lost $" + bet);
         if (betResult == WIN_BET)
             stake += 1;
         else
             stake -= 1;
     }
 
-    public void playTillMaxStake() {
+    public int playTillMaxStake() {
         while (stake < MAX_STAKE && stake > MIN_STAKE) {
             makeABet();
-            System.out.println("Stake = " + stake);
+            // System.out.println("Stake = " + stake);
+        }
+        return stake;
+    }
+
+    public void playMaxDays() {
+        int day = 0;
+        int currentDayStake;
+        while (day < MAX_DAYS) {
+            currentDayStake = playTillMaxStake();
+            day++;
+            stake = 100;
+            if (currentDayStake == MAX_STAKE) {
+                int wonStake = currentDayStake-stake;
+                System.out.println("You won at day " + day + " $" + wonStake);
+            } else {
+                int lostStake = stake-currentDayStake;
+                System.out.println("You lost at day " + day + " $" + lostStake);
+            }
         }
     }
 }
