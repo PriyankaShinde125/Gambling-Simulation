78037c7ea6cecdd732b21922296018431bd20872
“PriyankaShinde” <pkshinde125@gmail.com>
2022-12-03 14:31:13 +0530

    ADDED : Code to ask winner wants to play again or stop

--- src/Gamble.java
+++ src/Gamble.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class Gamble {
     public static final int WIN_BET = 1;
     public static final int MAX_DAYS = 20;
-    public final int MAX_STAKE;
-    public final int MIN_STAKE;
     public static final String DAYS_WON_KEY = "Won";
     public static final String DAYS_LOST_KEY = "Lost";
+    public final int maxStake;
+    public final int minStake;
     int stake;
     int bet;
     int monthlyWonDays = 0;
@@ -16,34 +16,39 @@ public class Gamble {
     int unLuckiestDay;
     HashMap<String, ArrayList<Integer>> daysWonLost;
     HashMap<Integer, Integer> dayAndAmountWon;
+    boolean isWon;
 
+    //Intialized the simulator with intial values
     public Gamble() {
         stake = 100;
         bet = 1;
-        MIN_STAKE = stake * 50 / 100;
-        MAX_STAKE = stake + MIN_STAKE;
+        minStake = stake * 50 / 100;
+        maxStake = stake + minStake;
         daysWonLost = new HashMap<>();
         daysWonLost.put(DAYS_WON_KEY, new ArrayList<>());
         daysWonLost.put(DAYS_LOST_KEY, new ArrayList<>());
         dayAndAmountWon = new HashMap<>();
     }
 
+    //Function to make a single bet and update stake
     public void makeABet() {
         int betResult = (int) Math.floor(Math.random() * 2);
         if (betResult == WIN_BET)
-            stake += 1;
+            stake += bet;
         else
-            stake -= 1;
+            stake -= bet;
     }
 
+    //Function to gamble until maximum win or maximum loss limit
     public int playTillMaxStake() {
-        while (stake < MAX_STAKE && stake > MIN_STAKE) {
+        while (stake < maxStake && stake > minStake) {
             makeABet();
         }
         return stake;
     }
 
-    public void playMaxDays() {
+    //Function to gamble daily for a month
+    public boolean playMaxDays() {
         int day = 0;
         int currentDayStake;
         while (day < MAX_DAYS) {
@@ -53,8 +58,10 @@ public class Gamble {
             dailyWinLoss(day, currentDayStake);
         }
         printMonthResult();
+        return isWon;
     }
 
+    //Function to print report for a month
     private void printMonthResult() {
         System.out.println("Total won days in month = " + monthlyWonDays + "\nWon days are as follows : ");
         System.out.println(daysWonLost.get(DAYS_WON_KEY));
@@ -62,34 +69,42 @@ public class Gamble {
         System.out.println(daysWonLost.get(DAYS_LOST_KEY));
 
         if (totalWonLostAmount > 0) {
+            isWon = true;
             System.out.println("You won by amount = " + totalWonLostAmount);
-            luckiestDay = 1;
-            unLuckiestDay = 1;
-            int maxWon = dayAndAmountWon.get(1);
-            int maxLost = maxWon;
 
-            for (Map.Entry<Integer, Integer> entry : dayAndAmountWon.entrySet()) {
-                if (entry.getValue() > maxWon) {
-                    maxWon = entry.getValue();
-                    luckiestDay = entry.getKey();
-                }
-                if (entry.getValue() < maxLost) {
-                    maxLost = entry.getValue();
-                    unLuckiestDay = entry.getKey();
-                }
-                System.out.println(entry.getKey() +" -> "+ entry.getValue());
-            }
-
-            System.out.println("Luckiest day = "+luckiestDay);
-            System.out.println("Unluckiest day = "+unLuckiestDay);
-
-        } else
+            findLuckiestUnluckiesDay();
+            // System.out.println(entry.getKey() + " -> " + entry.getValue());
+            System.out.println("Luckiest day = " + luckiestDay);
+            System.out.println("Unluckiest day = " + unLuckiestDay);
+        } else {
             System.out.println("Ooh.... you lost all amount");
+            isWon = false;
+        }
     }
 
+    //Function to find luckiest and unluckiest day
+    private void findLuckiestUnluckiesDay() {
+        luckiestDay = 1;
+        unLuckiestDay = 1;
+        int maxWon = dayAndAmountWon.get(1);
+        int maxLost = maxWon;
+
+        for (Map.Entry<Integer, Integer> entry : dayAndAmountWon.entrySet()) {
+            if (entry.getValue() > maxWon) {
+                maxWon = entry.getValue();
+                luckiestDay = entry.getKey();
+            }
+            if (entry.getValue() < maxLost) {
+                maxLost = entry.getValue();
+                unLuckiestDay = entry.getKey();
+            }
+        }
+    }
+
+    //Function to track daily win loss status and stake
     private void dailyWinLoss(int day, int currentDayStake) {
         int wonOrLostStake = Math.abs(currentDayStake - stake);
-        if (currentDayStake == MAX_STAKE) {
+        if (currentDayStake == maxStake) {
             daysWonLost.get(DAYS_WON_KEY).add(day);
             monthlyWonDays++;
             totalWonLostAmount += wonOrLostStake;
@@ -100,5 +115,16 @@ public class Gamble {
             totalWonLostAmount -= wonOrLostStake;
             dayAndAmountWon.put(day, totalWonLostAmount);
         }
+    }
+
+    //Function to reset gamble for new gamble
+    public void resetGame() {
+        totalWonLostAmount = 0;
+        monthlyLostDays = 0;
+        monthlyWonDays = 0;
+        isWon = false;
+        dayAndAmountWon.values().clear();
+        daysWonLost.get(DAYS_WON_KEY).clear();
+        daysWonLost.get(DAYS_LOST_KEY).clear();
     }
 }
